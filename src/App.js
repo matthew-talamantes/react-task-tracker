b3bd79e9f6d93f1d82ded831faa1998f41d8847f
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2021-10-10 17:21:54 -0400

    Add Tasks component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import React from "react";
+
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  
+  const [tasks, setTasks] = React.useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+    ]);
 
   const name = 'Matthew'; 
+  
   return (
     <div className='container'>
       <Header title='Task Tracker' name={name} />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
