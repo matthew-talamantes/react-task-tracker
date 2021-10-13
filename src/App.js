6ee31ce5b3f8c394db032563aa8bc9daad36b1f1
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2021-10-13 11:11:35 -0400

    Add ability to fetch tasks, add and delete tasks

--- src/App.js
+++ src/App.js
@@ -6,38 +6,50 @@ import AddTask from "./components/AddTask";
 
 function App() {
   
-  const [tasks, setTasks] = React.useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-    ]);
+  const [tasks, setTasks] = React.useState([]);
 
     const [showAddTask, setShowAddTask] = React.useState(false);
 
+    React.useEffect(() => {
+      const getTasks = async () => {
+        const tasksFromServer = await fetchTasks();
+        setTasks(tasksFromServer);
+      };
+
+      getTasks();
+    }, []);
+
+    // Fetch Tasks
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks');
+      const data = await res.json();
+      return data;
+    };
+
     // Add Task
-    const addTask = (task) => {
-      const id = Math.floor(Math.random() * 10000) + 1;
-      const newTask = {id, ...task};
-      setTasks([ ...tasks, newTask]);
+    const addTask = async (task) => {
+      // const id = Math.floor(Math.random() * 10000) + 1;
+      // const newTask = {id, ...task};
+      // setTasks([ ...tasks, newTask]);
+      const res =await fetch('http://localhost:5000/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      });
+
+      // const data = await fetchTasks();
+      const data = await res.json();
+      setTasks([...tasks, data]);
+
     };
 
     // Delete Task
-    const deleteTask = (id) => {
+    const deleteTask = async (id) => {
+      await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'DELETE',
+      });
       setTasks(tasks.filter((task) => task.id !== id))
     };
 
